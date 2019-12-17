bfbb3371631a6ffe5346432a40ab46f2b2918440
Zaccary Price <wildtangomedia@gmail.com>
2019-12-17 12:24:19 +0000

    add commands for options

--- rofi-power-menu.sh
+++ rofi-power-menu.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-# Starts a scan of available broadcasting SSIDs
-# nmcli dev wifi rescan
-
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 POSITION=0
@@ -21,9 +18,18 @@ else
 fi
 
 LINES="Lock screen|Sleep|Log out|Power off|Restart"
-
-echo -e "$LINES"
-
 CHENTRY=$(echo "$LINES" | rofi -dmenu -no-custom -p "Power" -sep '|' -location "$POSITION" -yoffset "$YOFF" -xoffset "$XOFF" -font "$FONT")
 
-echo "$CHENTRY"
+if [ "$CHENTRY" = "Lock screen" ]; then
+  betterlockscreen -l
+elif [ "$CHENTRY" = "Sleep"  ]; then
+  betterlockscreen -s
+elif [ "$CHENTRY" = "Log out"  ]; then
+  betterlockscreen -s
+elif [ "$CHENTRY" = "Power off"  ]; then
+  shutdown -h now
+elif [ "$CHENTRY" = "Restart"  ]; then
+  reboot
+else
+  echo "err"
+fi
